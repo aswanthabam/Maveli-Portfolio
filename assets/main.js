d791313efd35fa9c73ca71ef256814af3f8105ab
aswanthabam <aswanthabam@gmail.com>
2023-08-29 19:56:36 +0530

    main

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+document.getElementById("menu-btn").onclick = function() {
+  document.getElementsByClassName("sidemenu")[0].style.left = "0px";
+};
+document.getElementById("menu-btn-close").onclick = function () {
+  document.getElementsByClassName("sidemenu")[0].style.left="-100%";
+};
