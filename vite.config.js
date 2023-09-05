62cae9011f8e3284935240c377b62b8d1c6b4ee7
aswanthabam <aswanth.abam@gmail.com>
2023-09-05 21:15:17 +0530

    Hosting issue resolved

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ export default {
     host: "0.0.0.0",
     port: 3000,
   },
-  base:''
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html", // Specify your main entry point (index.html).
+        about: "./about.html", // Add other HTML files as separate entry points.
+        projects: "./projects.html", // Add more as needed.
+        contact: "./contact.html",
+      },
+    },
+  },
+  base: "",
 };
